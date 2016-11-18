287120f5db669f7d0c4e79326a6ff5890a0050e9
Alec Haroutonian <alecharoutonian@icloud.com>
2016-11-18 13:56:20 -0800

    added  more to my project

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,81 @@
 var turn ="player1"
-function circletwo(){
+function pink1(){
   if(turn=="player1"){
 circletwo
 turn="player2"
 }else{
-function circleone
+function blue1(){
   turn="player1"
 }
-var namespace = "http://www.w3.org/2000/svg"
+var turn ="player1"
+function pink2(){
+  if(turn=="player1"){
+circletwo
+turn="player2"
+}else{
+function blue2(){
+  turn="player1"
+}
+var turn ="player1"
+function pink3(){
+  if(turn=="player1"){
+circletwo
+turn="player2"
+}else{
+function blue3(){
+  turn="player1"
+}
+var turn ="player1"
+function pink4(){
+  if(turn=="player1"){
+circletwo
+turn="player2"
+}else{
+function blue4(){
+  turn="player1"
+}
+var turn ="player1"
+function pink5(){
+  if(turn=="player1"){
+circletwo
+turn="player2"
+}else{
+function blue5(){
+  turn="player1"
+}
+var turn ="player1"
+function pink6(){
+  if(turn=="player1"){
+circletwo
+turn="player2"
+}else{
+function blue6(){
+  turn="player1"
+}
+var turn ="player1"
+function pink7(){
+  if(turn=="player1"){
+circletwo
+turn="player2"
+}else{
+function blue7(){
+  turn="player1"
+}
+var turn ="player1"
+function pink8(){
+  if(turn=="player1"){
+circletwo
+turn="player2"
+}else{
+function blue8(){
+  turn="player1"
+}
+var turn ="player1"
+function pink9(){
+  if(turn=="player1"){
+circletwo
+turn="player2"
+}else{
+function blue9(){
+  turn="player1"
+}
