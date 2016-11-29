110e6a6551b5d210adb770824cc06bba885bd224
Alec Haroutonian <alecharoutonian@icloud.com>
2016-11-28 16:13:26 -0800

    Revert "worrked on functions and js"
    This reverts commit c5701f03e63874739098d9368725d0d2ed5327f3.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,17 +7,15 @@ turn="player2"
 function blue1(){
   turn="player1"
 }
-
 var turn ="player1"
 function pink2(){
   if(turn=="player1"){
 circletwo
-turn="player1"
+turn="player2"
 }else{
 function blue2(){
-  turn="player2"
+  turn="player1"
 }
-
 var turn ="player1"
 function pink3(){
   if(turn=="player1"){
@@ -27,8 +25,7 @@ turn="player2"
 function blue3(){
   turn="player1"
 }
-
-var turn ="player2"
+var turn ="player1"
 function pink4(){
   if(turn=="player1"){
 circletwo
@@ -37,7 +34,6 @@ turn="player2"
 function blue4(){
   turn="player1"
 }
-
 var turn ="player1"
 function pink5(){
   if(turn=="player1"){
@@ -47,7 +43,6 @@ turn="player2"
 function blue5(){
   turn="player1"
 }
-
 var turn ="player1"
 function pink6(){
   if(turn=="player1"){
@@ -57,7 +52,6 @@ turn="player2"
 function blue6(){
   turn="player1"
 }
-
 var turn ="player1"
 function pink7(){
   if(turn=="player1"){
@@ -67,7 +61,6 @@ turn="player2"
 function blue7(){
   turn="player1"
 }
-
 var turn ="player1"
 function pink8(){
   if(turn=="player1"){
@@ -77,7 +70,6 @@ turn="player2"
 function blue8(){
   turn="player1"
 }
-
 var turn ="player1"
 function pink9(){
   if(turn=="player1"){
