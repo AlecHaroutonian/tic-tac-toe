b4c662662cc3aaf987b3e0c8588166a7c3f1cba2
Alec Haroutonian <alecharoutonian@icloud.com>
2016-12-21 13:13:44 -0800

    top left working!

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,81 +1,93 @@
 var turn ="player1"
 function pink1(){
   if(turn=="player1"){
-circletwo
+    var pink1 = document.getElementById("pink1")
+    pink1.setAttribute("fill", "pink")
 turn="player2"
 }else{
-function blue1(){
+  var blue1 = document.getElementById("blue1")
+  blue1.setAttribute("fill", "blue")
   turn="player1"
 }
+}
+
+
+
 var turn ="player1"
 function pink2(){
   if(turn=="player1"){
-circletwo
+    var pink1 = document.getElementById("pink1")
+    pink1.setAttribute("fill", "pink")
 turn="player2"
 }else{
-function blue2(){
-  turn="player1"
 }
+}
+
+
+
 var turn ="player1"
 function pink3(){
   if(turn=="player1"){
-circletwo
+
 turn="player2"
 }else{
-function blue3(){
-  turn="player1"
 }
+}
+
 var turn ="player1"
 function pink4(){
   if(turn=="player1"){
-circletwo
+  // circletwo here!!!
 turn="player2"
 }else{
-function blue4(){
-  turn="player1"
 }
+}
+
 var turn ="player1"
 function pink5(){
   if(turn=="player1"){
-circletwo
+
 turn="player2"
 }else{
-function blue5(){
-  turn="player1"
 }
+}
+
+
 var turn ="player1"
 function pink6(){
   if(turn=="player1"){
-circletwo
+
 turn="player2"
 }else{
-function blue6(){
-  turn="player1"
 }
+}
+
+
 var turn ="player1"
 function pink7(){
   if(turn=="player1"){
-circletwo
+
 turn="player2"
 }else{
-function blue7(){
-  turn="player1"
 }
+}
+
+
 var turn ="player1"
 function pink8(){
   if(turn=="player1"){
-circletwo
+
 turn="player2"
 }else{
-function blue8(){
-  turn="player1"
 }
+}
+
+
 var turn ="player1"
 function pink9(){
   if(turn=="player1"){
-circletwo
+
 turn="player2"
 }else{
-function blue9(){
-  turn="player1"
+}
 }
